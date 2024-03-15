92287eb34bce73f1d665dbda049f384e0f99a1a8
Dhiraj Mistry <dhiraj@Dhirajs-MacBook-Air.local>
2024-03-15 13:46:47 +0530

    complte

---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import http from "http";
+import mongoose from "mongoose";
+import "dotenv/config";
+import routes from "./src/routes/index.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/api/v1", routes);
+
+const port = process.env.PORT || 5000;
+
+const server = http.createServer(app);
+
+// Connect to MongoDB
+
+
+// Define MongoDB URI
+const uri = 'mongodb://localhost:27017/your-database-name'; // Replace with your MongoDB URI
+
+// Connect to MongoDB
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
